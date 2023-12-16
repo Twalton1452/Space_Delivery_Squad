f6922cfc30db22066f93da1371b4db20c4082683
Tyler Walton <tyler.walton1452@gmail.com>
2023-12-16 14:18:28 -0600

    Add some models

--- scripts/DebugSpreader.gd
+++ scripts/DebugSpreader.gd
@@ -2,6 +2,9 @@
 extends Node3D
 
 @export_category("Debug")
+@export var x_spacing = 1
+@export var z_spacing = 1
+@export var row_size = 10
 @export var spread: bool:
 	set(value):
 		print("Spreading ", get_child_count(), " child objects")
@@ -10,12 +13,11 @@ extends Node3D
 func _on_spread() -> void:
 	var x = 1
 	var column = 0
-	var max_row_size = 10
 	for child in get_children():
-		child.position.x = x
+		child.position.x = x * x_spacing
 		child.position.y = 0
 		child.position.z = column
 		x += 1
-		if x % max_row_size == 0:
-			column += 1
+		if x % row_size == 0:
+			column += z_spacing
 			x = 0
