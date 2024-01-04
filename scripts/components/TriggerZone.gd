9171204e8880a883bbbdfb13caf6d60c4da18e8b
Tyler Walton <tyler.walton1452@gmail.com>
2024-01-04 14:10:03 -0600

    Add TriggerZones
    Players can walk through a TriggerZone to trigger something

---
+++ scripts/components/TriggerZone.gd
@@ -0,0 +1,20 @@
+@icon("res://art/icons/danger-circle.svg")
+extends Area3D
+class_name TriggerZone
+
+## Wrapper for Area3D to emit more specialized signals
+
+signal triggered(trigger_zone: TriggerZone, what_entered: CollisionObject3D)
+signal empty(trigger_zone: TriggerZone, what_exited: CollisionObject3D)
+
+func _ready() -> void:
+	body_entered.connect(_on_trigger_zone_entered)
+	body_exited.connect(_on_trigger_zone_exited)
+
+func _on_trigger_zone_entered(body) -> void:
+	if get_overlapping_bodies().size() == 1:
+		triggered.emit(self, body)
+
+func _on_trigger_zone_exited(body) -> void:
+	if get_overlapping_bodies().size() == 0:
+		empty.emit(self, body)
--- scripts/objects/Door.gd
+++ scripts/objects/Door.gd
@@ -4,7 +4,7 @@ class_name Door
 signal open
 signal close
 
-@export var triggers : Array[Interactable]
+@export var triggers : Array[Area3D]
 
 @export_category("Internal Scene Stuff")
 @export var collision_body : CollisionObject3D
@@ -15,9 +15,22 @@ var opened = false
 
 func _ready() -> void:
 	for trigger in triggers:
-		trigger.interacted.connect(_on_trigger)
+		if trigger is Interactable:
+			trigger.interacted.connect(_on_interactable_interacted)
+		elif trigger is TriggerZone:
+			trigger.triggered.connect(_on_trigger_zone_activated)
+			trigger.empty.connect(_on_trigger_zone_emptied)
 
-func _on_trigger(_interactable: Interactable, _interacter: Player) -> void:
+func _on_trigger_zone_activated(trigger_zone: TriggerZone, what_entered: CollisionObject3D) -> void:
+	_on_trigger(trigger_zone, what_entered)
+
+func _on_trigger_zone_emptied(trigger_zone: TriggerZone, what_exited: CollisionObject3D) -> void:
+	_on_trigger(trigger_zone, what_exited)
+
+func _on_interactable_interacted(interactable: Interactable, who_interacted: Player) -> void:
+	_on_trigger(interactable, who_interacted)
+
+func _on_trigger(_trigger: Area3D, _interacter: Player) -> void:
 	# Allows for interupting the animation cleanly
 	var current_seek = 0.0
 	if animation_player.is_playing():
