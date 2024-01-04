91aa5ceacd4c4b79c5cb1704b562b8a6de9dde90
Tyler Walton <tyler.walton1452@gmail.com>
2024-01-04 12:39:55 -0600

    begin depleting oxygen on power loss
    comes back on if mitigating an airlock disaster, temporary implementation until we utilize the DisasterManager

--- scripts/disasters/PowerLoss.gd
+++ scripts/disasters/PowerLoss.gd
@@ -8,7 +8,13 @@ func _ready() -> void:
 func _on_power_lost() -> void:
 	for child in get_tree().get_nodes_in_group("Lights").front().get_children():
 		(child as Light3D).hide()
+	
+	for player in PlayerManager.get_players():
+		player.turn_flags_on(Player.Flags.OXYGEN_DEPLETING)
 
 func _on_power_gained() -> void:
 	for child in get_tree().get_nodes_in_group("Lights").front().get_children():
 		(child as Light3D).show()
+	
+	for player in PlayerManager.get_players():
+		player.turn_flags_off(Player.Flags.OXYGEN_DEPLETING)
